455b57d724047e53df2e59b46e6c5c8965710b00
Suha Kopan <suha.kopan@huawei.com>
2020-09-29 11:58:58 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { IssueComponent } from './pages/issue/issue.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { AppLayoutComponent } from './_layout';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppLayoutComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      { path: 'issue', component: IssueComponent },
+      { path: 'project', component: ProjectComponent },
+      { path: 'dashboard', component: DashboardComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './_layout/footer/footer.component';
+import { SidebarComponent } from './_layout/sidebar/sidebar.component';
+import { HeaderComponent } from './_layout/header/header.component';
+import { AppLayoutComponent } from './_layout/app-layout/app-layout.component';
+
+import { ModalModule } from "ngx-bootstrap/modal";
+import { PaginationModule } from "ngx-bootstrap/pagination";
+import { CollapseModule } from "ngx-bootstrap/collapse";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import {ToastNoAnimation, ToastNoAnimationModule, ToastrModule} from "ngx-toastr";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SidebarComponent,
+    HeaderComponent,
+    AppLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CollapseModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    PaginationModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    ToastNoAnimationModule,
+    ToastrModule.forRoot({
+      toastComponent: ToastNoAnimation,
+      maxOpened: 1,
+      autoDismiss: true
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
